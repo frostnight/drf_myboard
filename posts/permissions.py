8f7128607497f4689be8715b441af7701e240236
frostnight <frostnightsong@gmail.com>
2022-10-17 03:55:48 +0900

    좋아요 기능 추가

--- posts/permissions.py
+++ posts/permissions.py
@@ -9,6 +9,6 @@ class CustomReadOnly(permissions.BasePermission):
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        if request.mehtod in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
