b1b2fa8dd3f20aa344def08a545f5e8535950543
frostnight <frostnightsong@gmail.com>
2022-10-20 04:59:35 +0900

    heroku 배포 수정

--- myboard/settings.py
+++ myboard/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = os.environ['secret_ket']
+SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
