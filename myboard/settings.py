27f962b35dd7bf50f1a9a1859c3b4694a41f01bc
frostnight <frostnightsong@gmail.com>
2022-10-20 05:00:54 +0900

    heroku 배포 수정

--- myboard/settings.py
+++ myboard/settings.py
@@ -14,6 +14,9 @@ import dj_database_url
 import environ
 from pathlib import Path
 
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
